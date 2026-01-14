1d30b9a7cac0814bd94cd80a84379dd0de390d92
Jojiya <jojiya12@gmail.com>
2026-01-14 11:07:30 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/',
-  plugins: [react()],
+  base: mode === 'production' ? './' : './',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 }))
