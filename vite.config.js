6c337897d8c6be79eb58ab2b7454d478cc2a796e
Jojiya <jojiya12@gmail.com>
2026-01-14 10:45:41 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
-  plugins: [react()]
+  base: '/Programer-Mcq-git/', // 👈 MUST match repo name
+  plugins: [react()],
 })
