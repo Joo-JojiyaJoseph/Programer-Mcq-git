41026cec9434598c0c504028baa2bf2ac983c6f2
Jojiya <jojiya12@gmail.com>
2026-01-15 15:08:36 +0530

    pwa updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/postcss'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? './' : './',
   plugins: [
-    react(),
-    tailwindcss(),
-  ],
+    react(),  tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'My React PWA',
+        short_name: 'ReactPWA',
+        description: 'My React Progressive Web App',
+        theme_color: '#0d6efd',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: './',
+        icons: [
+          {
+            src: './icon1.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './icon2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 }))
